9655351ee28a3bd9c322676748657b45859ae6e6
SeungHwan <seunghwanchoi@outlook.com>
2021-02-13 00:12:36 -0700

    two pointer method

--- main.cpp
+++ main.cpp
@@ -1,10 +1,48 @@
 #include <iostream>
+#include <vector>
+#include <string>
 
 using namespace std;
-    
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if(nums.empty())
+            return 0;
+        
+        //use two pointer method,
+        int p1 = 0;
+        int p2 = 0;
+
+        while(p2 < nums.size())
+        {
+            if(nums[p1] != nums[p2])
+            {
+                p1++;
+                nums[p1] = nums[p2];
+                
+            }
+            p2++;
+        }    
+        //resize vector.
+        nums.resize(p1 + 1);
+        return nums.size();
+    }
+};
+
 int main()
 {
-    cout << "hello world!" << endl;
+    
+    vector<int> nums = {0,0,1,1,1,2,2,3,3,4};
+
+    Solution obj;
+    int answer = obj.removeDuplicates(nums);
+
+    for(auto item : nums)
+    {
+        cout << item << ", ";
+    }
+    cout << endl;
         
     return 0;
 }
