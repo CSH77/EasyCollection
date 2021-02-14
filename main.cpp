1e4a90131d5ff8e6ef6a8696352e9c6116934466
SeungHwan <seunghwanchoi@outlook.com>
2021-02-14 03:28:25 -0700

    two pointer method

--- main.cpp
+++ main.cpp
@@ -1,11 +1,46 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
-    
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+
+        //use two pointer method,
+        if(prices.size() < 2)
+            return 0;
+
+        int total = 0;
+        int p1 = 0;
+        int p2 = 0;
+        while(p2 < prices.size())
+        {
+            //p2 is smaller than p1;
+            if(prices[p1] >= prices[p2])
+            {
+                p1 = p2;
+            }
+            else
+            {
+                total += (prices[p2] - prices[p1]);
+                p1 = p2;
+            }
+
+            p2++;
+        }
+
+        return total;
+    }
+};
+
 int main()
 {
-    cout << "hello world!" << endl;
-        
+    // vector<int> input = {7,1,5,3,6,4};
+    vector<int> input = {1,2,3,4,5};
+    Solution obj;
+    cout << obj.maxProfit(input);
+
     return 0;
 }
 
