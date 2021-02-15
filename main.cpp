7474362bbd3647a0e1d673837c18cb1c192dfcfa
SeungHwan <seunghwanchoi@outlook.com>
2021-02-15 04:00:45 -0700

    extra space used

--- main.cpp
+++ main.cpp
@@ -6,12 +6,53 @@
 
 using namespace std;
 
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        //rotate, use mod to calcualte rotating point
+        // int rotatingPoint = nums.size() % k;
+
+        vector<int> temp;
+         int rotatingPoint = k;
+        //rotation step is bigger than total size
+        if(k > nums.size())
+        {
+            rotatingPoint = k % nums.size();
+        }
+
+        //count from rear.
+        rotatingPoint = nums.size() - rotatingPoint;
+
+        int i = rotatingPoint;
+        while(i < nums.size())
+        {
+            temp.push_back(nums[i]);
+            i++;
+        }
+
+        i = 0;
+        while(i < rotatingPoint)
+        {
+            temp.push_back(nums[i]);
+            i++;
+        }
+        // nums.assign(temp.begin(), temp.end());
+        nums = temp;
+    }
+};
+
 int main()
 {
-    cout << "hello world!" << endl;
+    vector<int> input = {1,2,3,4,5,6,7};
+
+    Solution obj;
+    obj.rotate(input, 3);
+
+    for(auto item : input)
+    {
+        cout << item << " ";
+    }
+    cout << endl;
 
     return 0;
 }
-
-
-
