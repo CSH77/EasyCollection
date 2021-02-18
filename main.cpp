663d4d27e84b11842593aeb41702cb26b263ca25
SeungHwan <seunghwanchoi@outlook.com>
2021-02-17 23:39:25 -0700

    used hash table set

--- main.cpp
+++ main.cpp
@@ -3,12 +3,35 @@
 #include <vector>
 #include <queue>
 #include <map>
+#include <set>
 
 using namespace std;
 
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        //check if array contains duplicated value;
+
+        set<int> sset;
+        for(int i = 0; i < nums.size(); i++)
+        {
+            //find value from set, if it exist return true immediatly.
+            auto search = sset.find(nums[i]);
+            if( search != sset.end() )
+                return true;
+
+            sset.insert(nums[i]);
+        }
+
+        return false;
+    }
+};
+
 int main()
 {
-    cout << "hello world!" << endl;
+    vector<int> input = {1,1,1,3,3,4,3,2,4,2};
+    Solution obj;
+    cout << std::boolalpha << obj.containsDuplicate(input);
 
     return 0;
 }
